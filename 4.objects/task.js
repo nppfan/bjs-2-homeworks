d36c536cb1737db86cad971616aa7edcfd55b189
Gorelih Denis <nppfan96@mail.ru>
2024-06-04 22:50:01 +0500

    4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,35 @@
 function Student(name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
+    let StudentOne = new Student(this.name, this.gender, this.age, this.marks);
+    let StudentTwo = new Student(this.name, this.gender, this.age, this.marks);
+    let StudentThree = new Student(this.name, this.gender, this.age, this.marks);
+  }
   
-}
-
-Student.prototype.setSubject = function (subjectName) {
   
-}
-
-Student.prototype.addMarks = function (...marks) {
+  Student.prototype.setSubject = function (subjectName) {
+    this.subject = subjectName;
+  }
   
-}
-
-Student.prototype.getAverage = function () {
+  Student.prototype.addMarks = function (...marks) {
+    if (this.marks === undefined) {
+      return 0;
+    }
+    this.marks.push(...marks);
+  }
   
-}
-
-Student.prototype.exclude = function (reason) {
+  Student.prototype.getAverage = function () {
+    let sum = 0;
+    for (let i = 0; i < this.marks.length; i++) {
+      sum += this.marks[i];
+    }
+    return sum / this.marks.length;
+  }
   
-}
+  Student.prototype.exclude = function (reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+  }
