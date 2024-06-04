3ea70089721d2a3b0db8f1709ed6ca2884944cf9
Gorelih Denis <nppfan96@mail.ru>
2024-06-04 15:23:34 +0500

    3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,12 @@
 function compareArrays(arr1, arr2) {
+    return arr1.length === arr2.length && arr1.every((item, index) => item === arr2[index]);
+  }
   
-}
 
-function getUsersNamesInAgeRange(users, gender) {
-  
-}
+
+  function getUsersNamesInAgeRange(users, gender) {
+    let result = users.filter(user => user.gender === gender).map(user => user.age);
+    let sum = result.reduce((acc, age) => acc + age, 0);
+    let average = sum / result.length;
+    return average;
+  }
