3db6fea4bf18cc3430213e35fb49411af506059c
Gorelih Denis <nppfan96@mail.ru>
2024-06-04 13:12:29 +0500

    gkga

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,44 +1,44 @@
-// function getArrayParams(...arr) {
-// 	let min = -Infinity;
-// 	let max = Infinity;
-// 	let sum = 0;
-// 	for (i = 0; i < arr.length; i++) {
-// 		if (arr[i] > max) {
-// 			max = arr[i]
-// 		} else if (arr[i] < min) {
-// 			min = arr[i]
-// 		}
-// 		sum = arr[i];
-// 		avg = min + max + sum / 3;
-// 		avg.toFixed();
-// 		avg = Number(avg);
-// 	}
-// 	return {
-// 		min: min,
-// 		max: max,
-// 		avg: avg
-// 	};
-// }
-function getArrayParams(arr) {
-  let min, max, sum, avg;
-  min = Infinity;
-  max = -Infinity;
-
-  for (let i = 0; i < arr.length; i++) {
-      if (arr[i] > max) {
-          max = arr[i]
-      }
-      else if (arr[i] < min) {
-          min = arr[i]
-      }
-          sum = arr[i];
-          avg = min + max + sum / 3;
-          avg.toFixed();
-          avg = Number(avg);
-  }
-
-  return { min:min, max:max, avg:avg };
+function getArrayParams(...arr) {
+	let min = -Infinity;
+	let max = Infinity;
+	let sum = 0;
+	for (i = 0; i < arr.length; i++) {
+		if (arr[i] > max) {
+			max = arr[i]
+		} else if (arr[i] < min) {
+			min = arr[i]
+		}
+		sum = arr[i];
+		avg = min + max + sum / 3;
+		avg.toFixed();
+		avg = Number(avg);
+	}
+	return {
+		min: min,
+		max: max,
+		avg: avg
+	};
 }
+// function getArrayParams(arr) {
+//   let min, max, sum, avg;
+//   min = Infinity;
+//   max = -Infinity;
+
+//   for (let i = 0; i < arr.length; i++) {
+//       if (arr[i] > max) {
+//           max = arr[i]
+//       }
+//       else if (arr[i] < min) {
+//           min = arr[i]
+//       }
+//           sum = arr[i];
+//           avg = min + max + sum / 3;
+//           avg.toFixed();
+//           avg = Number(avg);
+//   }
+
+//   return { min:min, max:max, avg:avg };
+
 function summElementsWorker(...arr) {
 
 }
