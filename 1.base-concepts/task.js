def64f0f58a312d3d56ec5e78a314642cbcce541
Gorelih Denis <nppfan96@mail.ru>
2024-06-06 13:46:25 +0500

    1.2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,29 +1,19 @@
 function solveEquation(a, b, c) {
-	let arr = [];
-	let d = b ** 2 - 4 * a * c;
-
-
-	if (d === 0) {
-		arr = [-b / (2 * a)];
-	} else if (d > 0) {
-		arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
-	}
-	return arr;
+    let arr = [];
+    let d = b ** 2 - 4 * a * c;
+    if (d === 0) {
+        arr = [-b / (2 * a)];
+    } else if (d > 0) {
+        arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
+    }
+    return arr;
 }
 
-
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-	if (isNaN(percent) || isNaN(contribution) || isNaN(amount) || isNaN(countMonths)) {
-		return false;
-	}
-
-	percent = percent / 100 / 12;
-
-	let loanBody = amount - contribution;
-	let monthlyPayment = loanBody * (percent + (percent / ((Math.pow((1 + percent), countMonths)) - 1)));
-	let totalPayment = monthlyPayment * countMonths;
-
-	totalPayment = Number(totalPayment.toFixed(2));
-
-	return totalPayment;
+    let cent = percent / 100 / 12;
+    let loanBody = amount - contribution;
+    let monthlyPayment = loanBody * (cent + (cent / ((Math.pow((1 + cent), countMonths)) - 1)));
+    let totalPayment = monthlyPayment * countMonths;
+    totalPayment = Number(totalPayment.toFixed(2));
+    return totalPayment;
 }
