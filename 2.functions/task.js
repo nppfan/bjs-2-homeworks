23545e1b3cfee837e9c86d1e52758052c57d92aa
Gorelih Denis <nppfan96@mail.ru>
2024-06-25 14:50:39 +0500

    2,5

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,27 +1,24 @@
 function getArrayParams(...arr) {
-    let min = Infinity;
-    let max = -Infinity;
+    let max = Math.max(...arr);
+    let min = Math.min(...arr);
     let sum = 0;
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] > max) {
-            max = arr[i];
+            max = arr[i]
         } else if (arr[i] < min) {
-            min = arr[i];
+            min = arr[i]
         }
-        if (arr[i] > 0) {
-            sum += arr[i];
-        } else if (arr[i] < 0) {
-            sum += arr[i];
-        }
+        sum += arr[i];
     }
-    let avg = Number((sum / arr.length).toFixed(2));
+    avg = Number(avg.toFixed(2));
+    avg = sum / arr.length;
+    let avg = 0;
+    return {
+        min: min,
+        max: max,
+        avg: avg
+    };
 }
-return {
-    min: min,
-    max: max,
-    avg: avg
-};
-
 
 function summElementsWorker(...arr) {
 	let sum = 0;
