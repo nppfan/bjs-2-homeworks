6f59c70cd162acadc76579c7c0126787f44dcb44
Gorelih Denis <nppfan96@mail.ru>
2024-07-09 18:18:26 +0500

    3,3

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -3,6 +3,16 @@ function Student(name, gender, age) {
 	this.gender = gender;
 	this.age = age;
 	this.marks = [];
+  this.addMarks = function(...marks) {
+    if (!this.marks) {
+        console.log('Студент отчислен');
+        return;
+    }
+    if (marks.length === 0) {
+        return 0;
+    }
+    this.marks.push(...marks);
+}
 }
 
 let student1 = new Student('Иван', 'мужской', 20);
@@ -13,12 +23,23 @@ Student.prototype.setSubject = function(subjectName) {
 	this.subject = subjectName;
 }
 
+Student.prototype.calculateAverageMark = function() {
+  if (this.marks.length === 0) {
+      return 0;
+  }
+  const sum = this.marks.reduce((total, mark) => total + mark, 0);
+  return sum / this.marks.length;
+}
+
 Student.prototype.addMarks = function(...marks) {
   if (marks.length === 0) {
       return 0;
   }
   this.marks.push(...marks);
 }
+student1.addMarks(5, 4, 3);
+student2.addMarks(4, 4, 5);
+student3.addMarks(3, 3, 4);
 
 Student.prototype.getAverage = function() {
 	let sum = 0;
