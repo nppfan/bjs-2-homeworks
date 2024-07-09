2346d2fd63362b02f5e89ecf790b290db88a902c
Gorelih Denis <nppfan96@mail.ru>
2024-07-09 18:09:23 +0500

    3,3

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -14,10 +14,10 @@ Student.prototype.setSubject = function(subjectName) {
 }
 
 Student.prototype.addMarks = function(...marks) {
-	if (this.marks === undefined) {
-		return 0;
-	}
-	this.marks.push(...marks);
+  if (marks.length === 0) {
+      return 0;
+  }
+  this.marks.push(...marks);
 }
 
 Student.prototype.getAverage = function() {
