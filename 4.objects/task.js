211952c168d6777c9740dd550e56516171d91e17
Gorelih Denis <nppfan96@mail.ru>
2024-07-09 18:07:16 +0500

    3,3

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,37 +1,39 @@
 function Student(name, gender, age) {
-  this.name = name;
-  this.gender = gender;
-  this.age = age;
-  this.marks = [];
-  new Student.prototype.setSubject.apply(this, arguments);
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
-  
-  Student.prototype.setSubject = function (subjectName) {
-    this.subject = subjectName;
-  }
-  
-  Student.prototype.addMarks = function (...marks) {
-    if (this.marks === undefined) {
-      return 0;
-    }
-    this.marks.push(...marks);
-  }
-  
-  Student.prototype.getAverage = function () {
-    let sum = 0;
-    for (let i = 0; i < this.marks.length; i++) {
-      sum += this.marks[i];
-    }
-    if (this.marks.length === 0) {
-        return 0;
-      }
-    return sum / this.marks.length;
-  }
-  
-  Student.prototype.exclude = function (reason) {
-    delete this.subject;
-    delete this.marks;
-    this.excluded = reason;
-    
-  }
 
+let student1 = new Student('Иван', 'мужской', 20);
+let student2 = new Student('Мария', 'женский', 22);
+let student3 = new Student('Алексей', 'мужской', 21);
+
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
+}
+
+Student.prototype.addMarks = function(...marks) {
+	if (this.marks === undefined) {
+		return 0;
+	}
+	this.marks.push(...marks);
+}
+
+Student.prototype.getAverage = function() {
+	let sum = 0;
+	for (let i = 0; i < this.marks.length; i++) {
+		sum += this.marks[i];
+	}
+	if (this.marks.length === 0) {
+		return 0;
+	}
+	return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function(reason) {
+	delete this.subject;
+	delete this.marks;
+	this.excluded = reason;
+
+}
