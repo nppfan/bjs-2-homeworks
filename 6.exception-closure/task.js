82269ac391cd9d40b323a3f594e71c6ad0833f02
Gorelih Denis <nppfan96@mail.ru>
2024-07-25 16:08:50 +0500

    5,1

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -15,32 +15,31 @@ function validateCount(value) {
 }
 
 class Triangle {
-	constructor(a, b, c) {
-		this.a = a;
-		this.b = b;
-		this.c = c;
-    if (a + b <= c || a + c <= b || b + c <= a) {
-      throw new Error("Треугольник с такими сторонами не существует");
-    } 
-	}
-	get Perimeter() {
-		return this.a + this.b + this.c;
-	}
-	get Area() {
-		let p = this.Perimeter / 2;
-		return Number(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3));
-	}
+  constructor(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    if (a + b < c || a + c < b || b + c < a) {
+      throw new Error('Треугольник с такими сторонами не существует');
+    }
+  }
+  get perimeter() {
+    return this.a + this.b + this.c;
+  }
+  get area() {
+    let p = this.perimeter / 2;
+    return Number(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3));
+  }
 }
-
 function getTriangle(a, b, c) {
   try {
     return new Triangle(a, b, c);
   } catch (error) {
     return {
-      get Perimeter() {
+      get perimeter() {
         return 'Ошибка! Треугольник не существует';
       },
-      get Area() {
+      get area() {
         return 'Ошибка! Треугольник не существует';
       }
     }
