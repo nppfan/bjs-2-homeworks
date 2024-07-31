102b5d71159b9450ad0dd0150c976e00df6d658d
Gorelih Denis <nppfan96@mail.ru>
2024-07-31 15:27:50 +0500

    6,1

--- 7.async/task.js
+++ 7.async/task.js
@@ -1,55 +1,55 @@
 class AlarmClock {
     constructor() {
-      this.alarmCollection = [];
-      this.intervalId = null;
+        this.alarmCollection = [];
+        this.intervalId = null;
     }
-  
-    addClock(time, callback, id) {
-      if (id === undefined) {
-        throw new Error('error text');
-      }
-      if (this.alarmCollection.some(item => item.id === id)) {
-        console.error('Такой будильник уже существует');
-        return;
-      }
-      this.alarmCollection.push({
-        id,
-        time,
-        callback,
-        canCall: true
-      });
+
+    addClock(time, callback) {
+        if (!time || !callback) {
+            throw new Error('Отсутствуют обязательные аргументы');
+        }
+
+        if (this.alarmCollection.some(alarm => alarm.time === time)) {
+            console.warn('Уже присутствует звонок на это же время');
+            return;
+        }
+
+        this.alarmCollection.push({
+            time: time,
+            callback: callback,
+            canCall: true
+        });
     }
-  
+
     removeClock(time) {
-      this.alarmCollection = this.alarmCollection.filter(item => item.time !== time);
+        this.alarmCollection = this.alarmCollection.filter(item => item.time !== time);
     }
     getCurrentFormattedTime() {
-      return new Date().toLocaleTimeString().slice(0, 5);
+        return new Date().toLocaleTimeString().slice(0, 5);
     }
     start() {
-      if (this.intervalId === null) {
-        this.intervalId = setInterval(() => {
-          this.alarmCollection.forEach(item => {
-            if (item.time === this.getCurrentFormattedTime() && item.canCall) {
-              item.canCall = false;
-              item.callback();
-            }
-          });
-        });
-      }
+        if (this.intervalId === null) {
+            this.intervalId = setInterval(() => {
+                this.alarmCollection.forEach(item => {
+                    if (item.time === this.getCurrentFormattedTime() && item.canCall) {
+                        item.canCall = false;
+                        item.callback();
+                    }
+                });
+            }, 1000);
+        }
     }
     stop() {
-      if (this.intervalId !== null) {
-        clearInterval(this.intervalId);
-        this.intervalId = null;
-      }
+        if (this.intervalId !== null) {
+            clearInterval(this.intervalId);
+            this.intervalId = null;
+        }
     }
     resetAllCalls() {
-      this.alarmCollection.forEach(item => item.canCall = true);
+        this.alarmCollection.forEach(item => item.canCall = true);
     }
     clearAlarms() {
-      this.stop();
-      this.alarmCollection = [];
+        this.stop();
+        this.alarmCollection = [];
     }
-  }
-    
+}
